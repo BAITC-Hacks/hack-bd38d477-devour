d682323df22b8bee7892bb0afa97714cf264754e
Altusha4 <altynay.ertayit@gmail.com>
2026-09-23 13:55:41 +0500

    feat(engine): export event catalog from package

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,5 +1,6 @@
+from engine.events import get_event, list_events
 from engine.optimizer import optimize
 from engine.rules import load_data, validate
 from engine.scoring import simulate
 
-__all__ = ["load_data", "validate", "simulate", "optimize"]
+__all__ = ["load_data", "validate", "simulate", "optimize", "list_events", "get_event"]
